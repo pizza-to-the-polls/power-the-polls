09798488f5a183a62ee018707d877d7c71e83632
Scott Duncombe <scott.duncombe@zapier.com>
2020-08-02 23:51:29 -0700

    Create work elections component

--- site/src/components.d.ts
+++ site/src/components.d.ts
@@ -69,6 +69,20 @@ export namespace Components {
          */
         "url"?: string;
     }
+    interface WorkElections {
+        /**
+          * City for matching to location
+         */
+        "city": string;
+        /**
+          * County for matching to location
+         */
+        "county": string;
+        /**
+          * State for matching to location
+         */
+        "state": string;
+    }
 }
 declare global {
     interface HTMLAboutUsElement extends Components.AboutUs, HTMLStencilElement {
@@ -137,6 +151,12 @@ declare global {
         prototype: HTMLSocialShareElement;
         new (): HTMLSocialShareElement;
     };
+    interface HTMLWorkElectionsElement extends Components.WorkElections, HTMLStencilElement {
+    }
+    var HTMLWorkElectionsElement: {
+        prototype: HTMLWorkElectionsElement;
+        new (): HTMLWorkElectionsElement;
+    };
     interface HTMLElementTagNameMap {
         "about-us": HTMLAboutUsElement;
         "app-root": HTMLAppRootElement;
@@ -149,6 +169,7 @@ declare global {
         "privacy-info": HTMLPrivacyInfoElement;
         "ptp-faq": HTMLPtpFaqElement;
         "social-share": HTMLSocialShareElement;
+        "work-elections": HTMLWorkElectionsElement;
     }
 }
 declare namespace LocalJSX {
@@ -215,6 +236,20 @@ declare namespace LocalJSX {
          */
         "url"?: string;
     }
+    interface WorkElections {
+        /**
+          * City for matching to location
+         */
+        "city"?: string;
+        /**
+          * County for matching to location
+         */
+        "county"?: string;
+        /**
+          * State for matching to location
+         */
+        "state"?: string;
+    }
     interface IntrinsicElements {
         "about-us": AboutUs;
         "app-root": AppRoot;
@@ -227,6 +262,7 @@ declare namespace LocalJSX {
         "privacy-info": PrivacyInfo;
         "ptp-faq": PtpFaq;
         "social-share": SocialShare;
+        "work-elections": WorkElections;
     }
 }
 export { LocalJSX as JSX };
@@ -244,6 +280,7 @@ declare module "@stencil/core" {
             "privacy-info": LocalJSX.PrivacyInfo & JSXBase.HTMLAttributes<HTMLPrivacyInfoElement>;
             "ptp-faq": LocalJSX.PtpFaq & JSXBase.HTMLAttributes<HTMLPtpFaqElement>;
             "social-share": LocalJSX.SocialShare & JSXBase.HTMLAttributes<HTMLSocialShareElement>;
+            "work-elections": LocalJSX.WorkElections & JSXBase.HTMLAttributes<HTMLWorkElectionsElement>;
         }
     }
 }
