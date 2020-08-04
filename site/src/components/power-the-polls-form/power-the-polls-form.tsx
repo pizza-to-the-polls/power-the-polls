d8aaffd10fc8577f8362bb67d3050f53ac319bfd
Malachi Griffie <malachi@nexus.engineering>
2020-08-03 22:30:07 -0700

    removed dead code; refactoring

--- site/src/components/power-the-polls-form/power-the-polls-form.tsx
+++ site/src/components/power-the-polls-form/power-the-polls-form.tsx
@@ -55,18 +55,13 @@ export class PowerThePollsForm {
       cancelable: false,
    } ) public submitError!: EventEmitter<any>;
 
-   @State() private formComplete: boolean;
-   @State() private showStateFallback: boolean;
-   @State() private showPartner: boolean;
-   @State() private stateName?: string;
+   @State() private isFormComplete: boolean;
    @State() private city?: string;
    @State() private county?: string;
    @State() private state?: string;
 
    constructor() {
-      this.formComplete = false;
-      this.showStateFallback = false;
-      this.showPartner = false;
+      this.isFormComplete = false;
    }
 
    public render() {
@@ -88,15 +83,9 @@ export class PowerThePollsForm {
                ( {} as any ),
             );
             // hacky way to get the data from address-input without wiring up events or callbacks
-            const city: string = data.city || "";
-            const county: string = data.action_county || "";
-            const stateCode: string = data.state || "";
-
-            this.city = city;
-            this.county = county;
-            this.state = stateCode;
-            // query for work elections data so we can display the completion page with further information
-            // this.getWorkElectionsData( stateCode, city, county );
+            this.city = data.city || "";
+            this.county = data.action_county || "";
+            this.state = data.state || "";
 
             // submit to actionkit
             fetch( form.action, {
@@ -109,10 +98,10 @@ export class PowerThePollsForm {
             } )
                .then( ( response ) => {
                   if( response.status === 200 || response.status === 201
-                     || response.status === 0 /*we're currently makign a simple no-cors request and can't get the status*/ ) {
+                     || response.status === 0 /*we're currently making a simple no-cors request and can't get the status; presume success*/ ) {
                      let evt = this.submitCompleted.emit();
                      if( !evt.defaultPrevented ) {
-                        this.formComplete = true;
+                        this.isFormComplete = true;
                      }
                   } else {
                      response.json()
@@ -132,7 +121,7 @@ export class PowerThePollsForm {
       };
 
       return ( <Host>
-         {this.formComplete ? (
+         {this.isFormComplete ? (
             <article>
                <h1>Thanks for signing up to Power the Polls!</h1>
                <h2>We'll be in touch!</h2>
@@ -141,28 +130,11 @@ export class PowerThePollsForm {
                   <p><strong>Please note:</strong> You still need to complete an application for your community!</p>
                   <p>The application process for poll workers is different for every state, county, and voting territory. In the weeks ahead, Power the Polls will follow up via email and text message to make sure you complete the correct application for your community and are connected with your local election administrator.</p>
                </div>
-
                <poll-worker-info
                   city={this.city}
                   county={this.county}
                   state={this.state}
                />
-
-               {this.showStateFallback ? ( <Fragment>
-                  <p>
-                     Unfortunately it doesn't look like {this.stateName ? this.stateName : "your local election jurisdiction"} is signing up poll workers at this time.
-                  </p>
-                  <p>
-                     We are still collecting information compiling a list of locations that accept poll workers from other jurisdictions and will be in touch with other options when we're closer to the election.
-                  </p>
-               </Fragment> ) : null}
-
-               {this.showPartner ? ( <Fragment>
-                  <p>
-                     You'll hear from a partner election official or nonprofit soon about how you can help everyone in {this.stateName ? this.stateName : "your state"} vote.
-                  </p>
-               </Fragment> ) : null}
-
             </article>
          ) : ( <Fragment>
             <h3>Help democracy in your community. Sign up to Power the Polls.</h3>
@@ -246,71 +218,5 @@ export class PowerThePollsForm {
          </Fragment> )}
       </Host> );
    }
-
-   /*
-   private getWorkElectionsData( stateCode: string, city: string, county: string ){
-      let state = WorkElections[stateCode];
-      if( state == null ) {
-         let fallback = WorkElectionsFallbacks[stateCode];
-         if( fallback ) {
-            this.showStateFallback = true;
-            this.stateName = fallback;
-            analytics.signupLinkClickedFallback( "fallback_state", fallback );
-            // return ["fallback", fallback];
-         } else {
-            fallback = PartnerStates[stateCode];
-            if( fallback ) {
-               this.showPartner = true;
-               this.stateName = fallback;
-               analytics.signupLinkClickedFallback( "partner_state", fallback );
-               // return ["partner", fallback];
-            } else {
-               // return ["unknown", ""];
-            }
-         }
-      } else {
-         let jurisdictionCode = -1;
-         let matchingJurisdictionName = "";
-         if( state.jurisdictions[county + " County"] != null ) {
-            jurisdictionCode = state.jurisdictions[county + " County"];
-            matchingJurisdictionName = county;
-         }
-         if( state.jurisdictions[city + " (City)"] != null ) {
-            jurisdictionCode = state.jurisdictions[city + " (City)"];
-            matchingJurisdictionName = city;
-         }
-
-         if( jurisdictionCode === -1 ) {
-            let together = [city, "city,", county, "County"].join( " " );
-            if( state.jurisdictions[together] ) {
-               jurisdictionCode = state.jurisdictions[together];
-               matchingJurisdictionName = together;
-            }
-         }
-
-         analytics.signupLinkClicked( jurisdictionCode !== -1
-            ? `/j/${jurisdictionCode}/${matchingJurisdictionName.replace( /\s/g, "-" )}`
-            : `/states/${state.id}/${state.name}` );
-
-         if( jurisdictionCode !== -1 ) {
-            fetch( `https://workelections.powerthepolls.org/jurisdictions/${jurisdictionCode}/`, {
-               mode: "cors",
-               headers: {},
-            } )
-               .then( response => {
-                  if( response.status !== 200 ) {
-                     console.log( response.statusText, response.status );
-                  } else {
-                     response.json().then( ( data: JurisdictionQueryResponse ) => {
-                        this.jurisdiction = data;
-                     } );
-                  }
-               } )
-               .catch( err => console.log( "workelections query error", err ) );
-         }
-         // return ["found", path];
-      }
-   }
-   //*/
 
 }
