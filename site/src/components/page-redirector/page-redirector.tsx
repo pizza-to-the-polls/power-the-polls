a167999766d890f5850be82a09f66d2eadce959f
M. Griffie <mgriffie@nexus.engineering>
2020-08-13 20:16:08 -0700

    fixed fallback text for redirector on partner states

--- site/src/components/page-redirector/page-redirector.tsx
+++ site/src/components/page-redirector/page-redirector.tsx
@@ -1,4 +1,7 @@
+import { data } from "@ptp-us/power-the-polls-form";
 import { Component, h, Host } from "@stencil/core";
+
+import { Fragment } from "../../util/Fragment";
 
 @Component( {
    tag: "page-redirector",
@@ -6,13 +9,24 @@ import { Component, h, Host } from "@stencil/core";
 } )
 export class PageRedirector {
    public render() {
-      const hash = document.location.hash.replace("#", "");
 
-      const [state, county, city] = hash.split("?")[0].split("-").map(el => unescape(el));
+      const hash = document.location.hash.replace( "#", "" );
 
-      return (<Host>
+      const [state, county, city] = hash.split( "?" )[0].split( "-" ).map( el => unescape( el ) );
+
+      return ( <Host>
          <poll-worker-info city={city} county={county} state={state} />
-      </Host>);
+         {state != null && state in data.PartnerStates ?
+            (
+               <Fragment>
+                  <h2>{data.PartnerStates[state]}</h2>
+                  <p>
+                     You'll hear from a partner election official or nonprofit soon
+                     about how you can help everyone in {data.PartnerStates[state]} vote.
+                  </p>
+               </Fragment>
+            ) : null}
+      </Host> );
    }
 
 }
