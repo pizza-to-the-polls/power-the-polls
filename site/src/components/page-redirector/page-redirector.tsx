2a374fc8995609a0806c6f214249325cc523e6b1
M. Griffie <mgriffie@nexus.engineering>
2020-08-29 23:08:57 -0700

    fixed merge issues; version bump; updated text on redirector; fixed mobile size break

--- site/src/components/page-redirector/page-redirector.tsx
+++ site/src/components/page-redirector/page-redirector.tsx
@@ -16,16 +16,37 @@ export class PageRedirector {
 
       return ( <Host>
          <poll-worker-info city={city} county={county} state={state} />
-         {state != null && state in data.PartnerStates ?
-            (
-               <Fragment>
-                  <h2>{data.PartnerStates[state]}</h2>
+         {state != null && state in data.States.partners ?
+            ( <Fragment>
+               <h2>{data.States.partners[state]}</h2>
+               <p>
+                  We are sharing your information with election administrators and our state partners.
+               </p>
+               <p>
+                  You'll hear from a partner in the next week about how you can help serve as a poll worker in {data.States.partners[state]}.
+               </p>
+            </Fragment> )
+            : state != null && state in data.States.noPollWorkersNeeded ?
+               ( <Fragment>
+                  <h2>{data.States.noPollWorkersNeeded[state]}</h2>
+                  <p>Thank you so much for your interest in being a poll worker!</p>
                   <p>
-                     You'll hear from a partner election official or nonprofit soon
-                     about how you can help everyone in {data.PartnerStates[state]} vote.
+                     Good news: <strong>{data.States.noPollWorkersNeeded[state]} has indicated that they have all the election workers they need this year!</strong>
                   </p>
-               </Fragment>
-            ) : null}
+                  <p>
+                     The bad news is, that means we won’t have a place for you to serve as a poll worker, since your state is all set, and jurisdiction requirements unfortunately mean
+                     you won’t be eligible to serve in another state.
+                  </p>
+                  <p>
+                     We are passing your information on to your state's election administrators who will reach out if their needs change or if there are other
+                     opportunities to help their offices.
+                  </p>
+                  <p>
+                     <strong>You can still help power the polls</strong> by voting in this upcoming election, and encouraging your friends and family across the country to register
+                     to vote and, for those who live in other states - signing up to be poll workers.
+                  </p>
+               </Fragment> )
+               : null}
       </Host> );
    }
 
