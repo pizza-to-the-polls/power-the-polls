d52a25ba84c7c8de021b3e7190d0d22fdc230ca9
M. Griffie <mgriffie@nexus.engineering>
2020-09-09 20:11:33 -0700

    update page-redirector to support querystring args as well as hash and to redirect to /search on invalid jurisdiction

--- site/src/components/page-redirector/page-redirector.tsx
+++ site/src/components/page-redirector/page-redirector.tsx
@@ -1,6 +1,8 @@
 import { data, util } from "@ptp-us/power-the-polls-form";
 import { Component, h, Host } from "@stencil/core";
 
+import { getParams } from "../../util";
+
 @Component( {
    tag: "page-redirector",
    shadow: false,
@@ -8,10 +10,19 @@ import { Component, h, Host } from "@stencil/core";
 export class PageRedirector {
    public render() {
 
-      const hash = document.location.hash.replace( "#", "" );
-
-      const [state, county, city] = hash.split( "?" )[0].split( "-" ).map( el => unescape( el ) );
+      const query = getParams();
+      const hash = document.location.hash?.replace( "#", "" ) || query.source || "";
+      let [state, county, city] = hash.split( "?" )[0].split( "-" ).map( el => unescape( el ) );
+      if( !state ) {
+         state = query.state;
+         county = query.county;
+         city = query.city;
+      }
       const stateInfo = state != null && state in data.States ? data.States[state] : null;
+
+      if( !state ) {
+         return <stencil-router-redirect url="/search" />;
+      }
 
       return ( <Host>
          <ptp-info-poll-worker city={city} county={county} state={state} />
