d296a008ba690f0b4a6ee97a37da9778c3f7b431
M. Griffie <mgriffie@nexus.engineering>
2020-10-11 19:28:52 -0700

    updated ui-partner-image to allow a partnerID string

--- site/src/components/ui-partner-image/ui-partner-image.tsx
+++ site/src/components/ui-partner-image/ui-partner-image.tsx
@@ -1,5 +1,6 @@
-import { Component, h, Prop, State } from "@stencil/core";
+import { Component, Element, h, Listen, Prop, State, Watch } from "@stencil/core";
 
+import { PartnerList } from "../../data";
 import { Partner } from "../../data/types";
 import { REPO_NAME, REPO_ORG } from "../../util/constants";
 
@@ -15,15 +16,28 @@ export class UiPartnerImage {
       name: "Invalid",
    };
 
-   @Prop() public partner: Partner;
-   @Prop() public chosenPartner?: string;
+   /**
+    * A `Partner` object or partnerID string
+    */
+   @Prop() public partner: Partner | string;
+   /**
+    * If this value matches the `partner` ID then the image will have the `chosen-partner` class added which
+    * currently pulses the image.
+    */
+   @Prop() public chosenPartnerId?: string;
+   /**
+    * By default, an empty <span> with an id set to the partnerId is added to this component. If you don't want to
+    * pollute the ID space, you can exclude that span by setting this to `true`
+    */
    @Prop() public excludeAnchor: boolean;
    /**
     * If `true` the image will not be loaded from the deployed assets but from the `partner-updates` branch on GitHub
+    * @see page-partners-table
     */
    @Prop() public sourceFromDevBranch: boolean;
 
    @State() private loading: boolean;
+   @State() private partnerData!: Partner | null;
 
    constructor() {
       this.partner = UiPartnerImage.InvalidPartner;
@@ -32,32 +46,64 @@ export class UiPartnerImage {
       this.loading = true;
    }
 
+   public componentWillLoad() {
+      this.setPartnerData();
+   }
+
+   @Watch( "partner" )
+   public onPartnerChange( newVal: Partner | string ) {
+      this.setPartnerData( newVal );
+   }
+
+
    public render() {
-      const { partner, chosenPartner } = this;
+      const {
+         partnerData,
+         loading,
+         excludeAnchor,
+         chosenPartnerId,
+         sourceFromDevBranch } = this;
+
+      if( partnerData === null ) {
+         return ( <div class="loading">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
+         </div> );
+      }
+
       return (
          <div
             class={{
-               "background": partner.logoIsDark ?? false,
-               "chosen-partner": chosenPartner === partner.partnerId,
-            }}>
-            {!this.excludeAnchor &&
-               <span id={partner.partnerId} class="anchor"></span>
+               "background": partnerData.logoIsDark ?? false,
+               "chosen-partner": chosenPartnerId === partnerData.partnerId,
+            }}
+         >
+            {!excludeAnchor &&
+               <span id={partnerData.partnerId} class="anchor"></span>
             }
-            <img
-               src={partner.logo?.startsWith( "data:" )
-                  ? partner.logo
-                  : ( this.sourceFromDevBranch ? `https://raw.githubusercontent.com/${REPO_ORG}/${REPO_NAME}/partner-updates/site/public` : "" ) + `/assets/images/partners/${partner.logo}`}
-               title={partner.name}
-               onLoad={() => this.loading = false}
-            />
-            {this.loading &&
-               <div class={{ "loading": true, "dark": partner.logoIsDark ?? false }}>
+               <img
+                  src={partnerData.logo?.startsWith( "data:" )
+                     ? partnerData.logo
+                     // allow sourcing from git repo for page-partners-table
+                     : ( sourceFromDevBranch ? `https://raw.githubusercontent.com/${REPO_ORG}/${REPO_NAME}/partner-updates/site/public` : "" ) + `/assets/images/partners/${partnerData.logo}`}
+                  title={partnerData.name}
+                  onLoad={() => this.loading = false}
+               />
+            {loading &&
+               <div class={{ "loading": true, "dark": partnerData.logoIsDark ?? false }}>
                   <div class="double-bounce1"></div>
                   <div class="double-bounce2"></div>
                </div>
             }
          </div>
       );
+   }
+
+   private setPartnerData( data?: string | Partner ) {
+      data = data || this.partner;
+      this.partnerData = typeof data === "string"
+         ? [...PartnerList.filter( x => x.partnerId === data ), null][0]
+         : data;
    }
 
 }
